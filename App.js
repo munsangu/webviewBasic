0a3fa84494f090f06e30fee5b25bbf95b40ffe39
munsangu <ds-3632@hanmail.net>
2022-02-14 22:38:20 +0900

    20220214

--- App.js
+++ App.js
@@ -1,60 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet, BackHandler, Alert } from "react-native";
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
 
-const MYLINK = "https://relaxed-turing-e7ea57.netlify.app";
+const MYLINK = "https://midamsw.dbmanager.kr/account/login";
 
-// ios 이상없음, android 안됨
-// export default function App() {
-//   return (
-//     <View style={{ width: "100%", height: "100%" }}>
-//       <Text style={styles.text}>WEBVIEW why don't loading it?</Text>
-//       <View style={styles.container}>
-//         <WebView
-//           style={styles.test}
-//           onLoad={console.log("Looooood!")}
-//           source={{ url: MYLINK }}
-//         ></WebView>
-//       </View>
-//       <StatusBar style="auto"></StatusBar>
-//     </View>
-//   );
-// }
+// android 뒤로가기 버튼 클릭 시, 종료 확인 안내 문구 표시
+const App = () => {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Hold on!", "앱을 종료하시겠습니까?", [
+        {
+          text: "취소",
+          onPress: () => null,
+        },
+        { text: "확인", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginTop: Constants.statusBarHeight,
-//     // alignItems: "center",
-//     // justifyContent: "center",
-//     // borderColor: "#fff",
-//     // borderWidth: 2,
-//     // borderStyle: "solid",
-//   },
-//   text: {
-//     fontSize: 30,
-//     marginTop: Constants.statusBarHeight,
-//     marginBottom: 20,
-//     // fontWeight: "700",
-//     textAlign: "center",
-//     color: "#2598eb",
-//   },
-//   test: {
-//     flex: 1,
-//     marginTop: Constants.statusBarHeight,
-//   },
-// });
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
 
-// ios, android 둘 다 됨
-export default function App() {
-  return <WebView style={styles.container} source={{ uri: MYLINK }} />;
-}
+    return () => backHandler.remove();
+  }, []);
+
+  return (
+    <View style={{ width: "100%", height: "100%" }}>
+      <Text style={styles.text}>WEBVIEW</Text>
+      <View style={styles.container}>
+        <WebView
+          style={styles.test}
+          onLoad={console.log("Looooood!")}
+          source={{ uri: MYLINK }}
+        ></WebView>
+      </View>
+      <StatusBar style="auto"></StatusBar>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // marginTop: Constants.statusBarHeight,
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    // marginTop: Constants.statusBarHeight,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#2598eb",
+  },
+  test: {
+    flex: 1,
     marginTop: Constants.statusBarHeight,
   },
 });
+
+export default App;
